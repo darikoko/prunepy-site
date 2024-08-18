d5800769386bcc650489099e8e6039e8448a20f4
Loïc <contact@loic-rust.fr>
2024-08-18 20:57:57 +0200

    4 min to read

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/get-started/installation">
-            Tutorial - 5min ⏱️
+            Tutorial - 4min ⏱️
           </Link>
         </div>
       </div>
